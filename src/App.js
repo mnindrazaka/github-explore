b069954a8f050050a8908d35181858efb0ff0a56
mnindrazaka <mnindrazaka@gmail.com>
2018-05-29 11:19:32 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import { Layout } from 'antd'
+import { Layout, Menu, Icon } from 'antd'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import ExploreScene from './scenes/Explore'
+import AboutScene from './scenes/About'
 const { Header, Footer, Sider, Content } = Layout
+
+const SideMenu = () => (
+	<Menu mode="horizontal" style={{ lineHeight: '64px' }}>
+		<Menu.Item key="1">
+			<Link to="/">
+				<Icon type="github" />
+				Explore
+			</Link>
+		</Menu.Item>
+
+		<Menu.Item key="2">
+			<Link to="/about">
+				<Icon type="info-circle-o" />
+				About
+			</Link>
+		</Menu.Item>
+	</Menu>
+)
+
+const Router = () => (
+	<div>
+		<Route exact path="/" component={ExploreScene} />
+		<Route path="/about" component={AboutScene} />
+	</div>
+)
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<Layout>
-					<Sider>Sider</Sider>
+			<BrowserRouter>
+				<div>
 					<Layout>
-						<Header>Header</Header>
-						<Content>Content</Content>
-						<Footer>Footer</Footer>
+						<Header style={styles.header}>
+							<div style={styles.logo}>
+								<h2>Github Explorer</h2>
+							</div>
+							<SideMenu />
+						</Header>
+						<Layout>
+							<Content style={styles.content}>
+								<Router />
+							</Content>
+						</Layout>
 					</Layout>
-				</Layout>
-			</div>
+				</div>
+			</BrowserRouter>
 		)
 	}
+}
+
+const styles = {
+	header: {
+		background: '#fff',
+	},
+	logo: {
+		float: 'left',
+		marginRight: 25,
+	},
+	content: {
+		background: '#fff',
+	},
 }
 
 export default App
---
+++ src/scenes/Explore/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Explore extends Component {
+	render() {
+		return <div>Explore</div>
+	}
+}
